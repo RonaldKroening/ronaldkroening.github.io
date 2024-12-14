e2e0722b711bb893fd1822c6c790d74cbcf2a007
Ronald Kroening <rkroen01@tufts.edu>
2024-12-14 14:24:53 -0500

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Required for static export
+  output: 'export', // Use 'export' for static generation
   basePath: isProd ? '/ronaldkroening.github.io' : '',
   assetPrefix: isProd ? '/ronaldkroening.github.io/' : '',
 };
